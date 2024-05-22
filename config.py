3a4c79901ab91247c4c91d1a9985e108fa982dfc
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-22 12:41:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
+    BOT_TOKEN = "6501548699:AAEIsN52Q4ZZagVFO8HTO1XCKjuVON-qhpQ"
     API_ID = "20717621"
     API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
     AUTH_USERS = '5934830127,1085174050'
